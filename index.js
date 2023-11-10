5eb8649d23d614f1159be79a5b94fb7b474cf789
juandiego9221 <juandiego9221@gmail.com>
2023-11-10 18:20:55 -0500

    Add product and category routes

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ app.get('/example1', (req, res) => {
   res.send('example1');
 });
 
-app.get('/example2', (req, res) => {
-  console.log('example2');
-  // res.send('example2');
-  res.json({ name: 'example2', age: 20 });
+app.get('/products', (req, res) => {
+  const products = [{ name: 'product 1', price: 20 }, { name: 'product 2', price: 30 }, { name: 'product 3', price: 40 }];
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  // const id = req.params.id;
+  const { id } = req.params;
+  const product = { id, name: 'product 1', price: 20 };
+  res.json(product);
+});
+
+app.get("/categories/:categoryId/products/:productId", (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({ categoryId, productId });
 });
 
 app.listen(port, () => {
