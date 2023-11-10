f789a767e6407a87751ac5139389394644b8ca77
juandiego9221 <juandiego9221@gmail.com>
2023-11-10 18:11:56 -0500

    Add example1 and example2 routes.

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/example1', (req, res) => {
+  console.log('example1');
+  res.send('example1');
+});
+
+app.get('/example2', (req, res) => {
+  console.log('example2');
+  // res.send('example2');
+  res.json({ name: 'example2', age: 20 });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
