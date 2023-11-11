ea3baf165bd77ca3a8fd047cb6bca43ac1d3ef89
juandiego9221 <juandiego9221@gmail.com>
2023-11-11 15:43:20 -0500

    Add CORS middleware to allow requests from specified origins

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { faker } = require('@faker-js/faker');
 const routerApi = require("./routes")
+const cors = require('cors');
 
 // require error handler function from errorHandler.js
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/errorHandler');
@@ -9,6 +10,20 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ["http://localhost:8080", "https://myapp.co"]
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("No permitido"))
+    }
+  },
+}
+app.use(cors(options));
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
